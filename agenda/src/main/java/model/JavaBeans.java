d11a3476a5145c9330cad98ba657afabef0180a8
LuccasAguiarLCC <luccasaguiarlcc@gmail.com>
2024-02-25 12:35:56 -0300

    feat: Camada model - get/set e construtores

--- agenda/src/main/java/model/JavaBeans.java
+++ agenda/src/main/java/model/JavaBeans.java
@@ -1,5 +1,46 @@
 package model;
 
 public class JavaBeans {
+	private String idcon;
+	private String nome;
+	private String tel;
+	private String email;
+	
+	public JavaBeans() {
+		super();
+	}
+	
+	public JavaBeans(String idcon, String nome, String tel, String email) {
+		super();
+		this.idcon = idcon;
+		this.nome = nome;
+		this.tel = tel;
+		this.email = email;
+	}
 
+	public String getIdcon() {
+		return idcon;
+	}
+	public void setIdcon(String idcon) {
+		this.idcon = idcon;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 }
