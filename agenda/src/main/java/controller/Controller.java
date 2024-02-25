e69fe37e6d538b55894e3073f4d4f1f27c67b157
LuccasAguiarLCC <luccasaguiarlcc@gmail.com>
2024-02-24 21:36:00 -0300

    feat: Config servlet para receber req na camada view

--- agenda/src/main/java/controller/Controller.java
+++ agenda/src/main/java/controller/Controller.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 /**
  * Servlet implementation class Controller
  */
-@WebServlet("/Controller")
+@WebServlet(urlPatterns = {"/controller", "/main"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
